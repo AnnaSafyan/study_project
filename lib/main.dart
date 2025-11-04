59afadb3392b445ba65e69c0af19ecbfe2387735
HannaSafyan <hanna.safyan@innowise-group.com>
2025-11-04 19:29:25 +0300

    fixed app bar theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: LightAppTheme.lightTheme,
+      theme: DarkAppTheme.darkTheme,
+      // theme: LightAppTheme.lightTheme
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -41,10 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
